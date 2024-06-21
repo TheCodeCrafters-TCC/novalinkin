8d61e16bdda975bc82e404e7396a075d2c798033
Rapheal Chizitere <www.rashyking05@gmail.com>
2024-06-21 23:48:22 +0100

    File configs

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const { PORT, CONNECTION_URI } = process.env;
+export const { PORT, DB_URL } = process.env;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,16 @@
-import express from "express"
-import signUpController from "../controllers/index.js"
+import express from "express";
+import signUpController from "../controllers/index.js";
 import { signUpValidator } from "../validators/auth.js";
 import { signInValidator } from "../validators/auth.js";
 import validate from "../validators/validate.js";
-import { signInController } from "../controllers/auth.js";
+// import { signInController } from "../controllers/auth.js";
 
 export const router = express.Router();
 
-router.post("/sign-up", signUpValidator, validate, signUpController)
+router.post("/sign-up", signUpValidator, validate, signUpController);
 
-router.post("/sign-in", signInValidator, validate, signInController)
+// router.post("/sign-in", signInValidator, validate, signInController)
 
 router.get("/", (req, res, next) => {
-    res.send("Hello");
-})
+  res.send("Hello");
+});
