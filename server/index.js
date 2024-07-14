ae0af9acb56710b908694c526286b997ff48da5c
Rapheal Chizitere <www.rashyking05@gmail.com>
2024-07-14 03:13:48 +0100

    Onreader port binding

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import { PORT } from "./config/keys.js";
 import connectifyServer from "./app.js";
 
-connectifyServer.listen(PORT || 6000, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+const port = PORT || 5000;
+
+connectifyServer.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
 );
