0978a54937710c452d17abd761ec49c0b8edb7e8
Rapheal Chizitere <www.rashyking05@gmail.com>
2024-07-14 03:34:56 +0100

    open port error

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ import connectifyServer from "./app.js";
 
 const port = PORT || 4000;
 
+connectifyServer.get("/", (req, res) => {
+  console.log("Hello");
+  res.send("Hello");
+});
+
 connectifyServer.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
